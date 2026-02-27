6e701b8971df63a893ed9c77dd0cfb1ebf2f3d48
Glavo <zjx001202@gmail.com>
2026-02-28 00:41:07 +0800

    Update build configuration for publishing and Javadoc generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,22 @@ dependencies {
     testImplementation(libs.opennbt)
 }
 
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
 tasks.compileJava {
     options.release.set(17)
+}
+
+tasks.javadoc {
+    (options as StandardJavadocDocletOptions).also {
+        it.encoding("UTF-8")
+        it.addStringOption("link", "https://docs.oracle.com/en/java/javase/17/docs/api/")
+        it.addBooleanOption("html5", true)
+        it.addStringOption("Xdoclint:none", "-quiet")
+    }
 }
 
 tasks.test {
@@ -34,5 +48,42 @@ tasks.jacocoTestReport {
         xml.required.set(true)
         csv.required.set(true)
         html.required.set(true)
+    }
+}
+
+tasks.withType<GenerateModuleMetadata> {
+    enabled = false
+}
+
+publishing.publications.create<MavenPublication>("maven") {
+    groupId = project.group.toString()
+    version = project.version.toString()
+    artifactId = project.name
+
+    from(components["java"])
+
+    pom {
+        name.set(project.name)
+        description.set(project.description)
+        url.set("https://github.com/Glavo/HelloNBT")
+
+        licenses {
+            license {
+                name.set("Apache-2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("Glavo")
+                name.set("Glavo")
+                email.set("zjx001202@gmail.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/Glavo/HelloNBT")
+        }
     }
 }

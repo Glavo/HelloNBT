537088d3db85b4526aa4594be6c5fe5d2af0682a
Glavo <zjx001202@gmail.com>
2026-03-04 01:07:27 +0800

    Remove unused MethodHandles import from Tag

--- src/main/java/org/glavo/nbt/tag/Tag.java
+++ src/main/java/org/glavo/nbt/tag/Tag.java
@@ -29,7 +29,6 @@ import org.jetbrains.annotations.Nullable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.lang.invoke.MethodHandles;
 import java.util.Objects;
 
 /// @author Glavo

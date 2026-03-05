a1e92325b53bc684b1818c1cc5586477816c681a
Glavo <zjx001202@gmail.com>
2026-03-05 10:51:15 +0800

    Add CompressMethod sealed interface with built-in implementations

---
+++ src/main/java/org/glavo/nbt/io/CompressMethod.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2026 Glavo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.glavo.nbt.io;
+
+public sealed interface CompressMethod {
+    enum BuiltIn implements CompressMethod {
+        GZIP,
+        ZLIB,
+        UNCOMPRESSED,
+        LZ4
+    }
+
+    // TODO: Add support for custom compression methods
+    final class Custom implements CompressMethod {
+        private final String name;
+
+        public Custom(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return "Custom[" + name + "]";
+        }
+    }
+}

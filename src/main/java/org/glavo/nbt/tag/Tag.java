3cbff0bb3cd3e48bc4295419be3e8b349f0d3b42
Glavo <zjx001202@gmail.com>
2026-03-10 13:25:48 +0800

    Add @Contract annotation to Tag.getType()

--- src/main/java/org/glavo/nbt/tag/Tag.java
+++ src/main/java/org/glavo/nbt/tag/Tag.java
@@ -64,6 +64,7 @@ public sealed abstract class Tag implements NBTElement
     }
 
     /// Returns the type of the tag.
+    @Contract(pure = true)
     public abstract TagType<?> getType();
 
     /// Returns the name of the tag, or an empty string if it has no name.

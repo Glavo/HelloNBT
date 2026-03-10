5753fb20efadf231302ddae75b9ddf95cc53e021
Glavo <zjx001202@gmail.com>
2026-03-10 13:05:23 +0800

    Make TagType abstract with anonymous implementations

--- src/main/java/org/glavo/nbt/tag/TagType.java
+++ src/main/java/org/glavo/nbt/tag/TagType.java
@@ -18,69 +18,128 @@ package org.glavo.nbt.tag;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.function.Supplier;
 
 /// This class represents the type of [tag][Tag] in NBT (Named Binary Tag) format.
-public final class TagType<T extends Tag> {
+public abstract class TagType<T extends Tag> {
     /// 1 byte signed integer type. Sometimes used for booleans.
     ///
     /// @see ByteTag
-    public static final TagType<ByteTag> BYTE = new TagType<>("TAG_Byte", 0x01, ByteTag.class, ByteTag::new);
+    public static final TagType<ByteTag> BYTE = new TagType<>("TAG_Byte", 0x01, ByteTag.class) {
+        @Override
+        public ByteTag createTag() {
+            return new ByteTag();
+        }
+    };
 
     /// 2 byte signed integer type.
     ///
     /// @see ShortTag
-    public static final TagType<ShortTag> SHORT = new TagType<>("TAG_Short", 0x02, ShortTag.class, ShortTag::new);
+    public static final TagType<ShortTag> SHORT = new TagType<>("TAG_Short", 0x02, ShortTag.class) {
+        @Override
+        public ShortTag createTag() {
+            return new ShortTag();
+        }
+    };
 
     /// 4 byte signed integer type.
     ///
     /// @see IntTag
-    public static final TagType<IntTag> INT = new TagType<>("TAG_Int", 0x03, IntTag.class, IntTag::new);
+    public static final TagType<IntTag> INT = new TagType<>("TAG_Int", 0x03, IntTag.class) {
+        @Override
+        public IntTag createTag() {
+            return new IntTag();
+        }
+    };
 
     /// 8 byte signed integer type.
     ///
     /// @see LongTag
-    public static final TagType<LongTag> LONG = new TagType<>("TAG_Long", 0x04, LongTag.class, LongTag::new);
+    public static final TagType<LongTag> LONG = new TagType<>("TAG_Long", 0x04, LongTag.class) {
+        @Override
+        public LongTag createTag() {
+            return new LongTag();
+        }
+    };
 
     /// 4 byte floating point type.
     ///
     /// @see FloatTag
-    public static final TagType<FloatTag> FLOAT = new TagType<>("TAG_Float", 0x05, FloatTag.class, FloatTag::new);
+    public static final TagType<FloatTag> FLOAT = new TagType<>("TAG_Float", 0x05, FloatTag.class) {
+        @Override
+        public FloatTag createTag() {
+            return new FloatTag();
+        }
+    };
 
     /// 8 byte floating point type.
     ///
     /// @see DoubleTag
-    public static final TagType<DoubleTag> DOUBLE = new TagType<>("TAG_Double", 0x06, DoubleTag.class, DoubleTag::new);
+    public static final TagType<DoubleTag> DOUBLE = new TagType<>("TAG_Double", 0x06, DoubleTag.class) {
+        @Override
+        public DoubleTag createTag() {
+            return new DoubleTag();
+        }
+    };
 
     /// An array of bytes.
     ///
     /// @see ByteArrayTag
-    public static final TagType<ByteArrayTag> BYTE_ARRAY = new TagType<>("TAG_Byte_Array", 0x07, ByteArrayTag.class, ByteArrayTag::new);
+    public static final TagType<ByteArrayTag> BYTE_ARRAY = new TagType<>("TAG_Byte_Array", 0x07, ByteArrayTag.class) {
+        @Override
+        public ByteArrayTag createTag() {
+            return new ByteArrayTag();
+        }
+    };
 
     /// A UTF-8 encoded string. It has a size, rather than being null terminated.
     ///
     /// @see StringTag
-    public static final TagType<StringTag> STRING = new TagType<>("TAG_String", 0x08, StringTag.class, StringTag::new);
+    public static final TagType<StringTag> STRING = new TagType<>("TAG_String", 0x08, StringTag.class) {
+        @Override
+        public StringTag createTag() {
+            return new StringTag();
+        }
+    };
 
     /// A list of tag payloads, without tag IDs or names, apart from the one before the length.
     ///
     /// @see ListTag
-    public static final TagType<ListTag<?>> LIST = new TagType<>("TAG_List", 0x09, ListTag.class, ListTag::new);
+    public static final TagType<ListTag<?>> LIST = new TagType<>("TAG_List", 0x09, ListTag.class) {
+        @Override
+        public ListTag<?> createTag() {
+            return new ListTag<>();
+        }
+    };
 
     /// A list of fully formed tags, including their IDs, names, and payloads. No two tags may have the same name.
     ///
     /// @see CompoundTag
-    public static final TagType<CompoundTag> COMPOUND = new TagType<>("TAG_Compound", 0x0A, CompoundTag.class, CompoundTag::new);
+    public static final TagType<CompoundTag> COMPOUND = new TagType<>("TAG_Compound", 0x0A, CompoundTag.class) {
+        @Override
+        public CompoundTag createTag() {
+            return new CompoundTag();
+        }
+    };
 
     /// An array of 4 byte signed integers.
     ///
     /// @see IntArrayTag
-    public static final TagType<IntArrayTag> INT_ARRAY = new TagType<>("TAG_Int_Array", 0x0B, IntArrayTag.class, IntArrayTag::new);
+    public static final TagType<IntArrayTag> INT_ARRAY = new TagType<>("TAG_Int_Array", 0x0B, IntArrayTag.class) {
+        @Override
+        public IntArrayTag createTag() {
+            return new IntArrayTag();
+        }
+    };
 
     /// An array of 8 byte signed integers.
     ///
     /// @see LongArrayTag
-    public static final TagType<LongArrayTag> LONG_ARRAY = new TagType<>("TAG_Long_Array", 0x0C, LongArrayTag.class, LongArrayTag::new);
+    public static final TagType<LongArrayTag> LONG_ARRAY = new TagType<>("TAG_Long_Array", 0x0C, LongArrayTag.class) {
+        @Override
+        public LongArrayTag createTag() {
+            return new LongArrayTag();
+        }
+    };
 
     /// Returns the tag type by its id; returns `null` if the id is invalid.
     public static @Nullable TagType<?> getById(byte id) {
@@ -109,14 +168,12 @@ public final class TagType<T extends Tag> {
     private final String name;
     private final byte id;
     private final Class<T> tagClass;
-    private final Supplier<T> tagSupplier;
 
     @SuppressWarnings("unchecked")
-    private TagType(String name, int id, Class<?> tagClass, Supplier<T> tagSupplier) {
+    private TagType(String name, int id, Class<?> tagClass) {
         this.name = name;
         this.id = (byte) id;
         this.tagClass = (Class<T>) tagClass;
-        this.tagSupplier = tagSupplier;
     }
 
     public String name() {
@@ -135,9 +192,7 @@ public final class TagType<T extends Tag> {
         return tagClass;
     }
 
-    public T createTag() {
-        return tagSupplier.get();
-    }
+    public abstract T createTag();
 
     @Override
     public String toString() {
